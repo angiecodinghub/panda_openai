5e05750f8a7a2efeeb40c10c642732fd04f45412
angiecodinghub <angelawu625@gmail.com>
2022-03-28 17:48:55 -0500

    upload 7 joint model and 7 joint test

--- scripts/step_test.py
+++ scripts/step_test.py
@@ -14,30 +14,35 @@ import time
 import numpy as np
 
 if __name__ == '__main__':
-    print("before init node")
-    rospy.init_node('step_test', anonymous = True, log_level=rospy.DEBUG)
-    print("after init node")
+    # init ROS node.
+    rospy.init_node('step_test', anonymous = True, log_level=rospy.INFO)
+
     # Create the Gym environment
-    env = gym.make('PandaReach-v2')
-    rospy.loginfo("Gym environment made")
+    env = gym.make('PandaReach-v2', control_type = "joint")  # ee (action space: 3) or joint (action space: 7)
+    rospy.loginfo("MADE ENVIRONMENT")
 
     # Load trained model
     model_class = DDPG
-    model = model_class.load("/home/panda/catkin_ws/src/panda_reach_test/models/reach_vec", env = env)
-    rospy.loginfo("Trained model loaded")
+    model = model_class.load("/home/panda/catkin_ws/src/panda_reach_test/models/reach_joint", env = env)
+    rospy.loginfo("LOADED MODEL")
 
     # start outputting path.
-    obs = env.reset() # change this to init position?
+    obs = env.reset() # init position will be the one with 90 degree at the elbow.
     done = False
-    action = np.array([0.0032298252917826176, -0.14821836352348328, 0.9403465390205383])
-    obs, reward, done, _ = env.step(action)
-    #print(obs)
-    # Failed to validate trajectory: couldn't receive full current joint state within 1s
-    # Failed to fetch current robot state.
-    """
+    rospy.loginfo("GOAL OBSERVATION:")
+    rospy.loginfo(obs["desired_goal"])
+    rospy.loginfo("START OBSERVATION:")
+    rospy.loginfo(obs['observation'])
     while not done:
-        # action, _ = model.predict(obs, deterministic = True) # unexpected observation shape
-        action = env.action_space.sample()
+        action, _ = model.predict(obs, deterministic = True)
         obs, reward, done, _ = env.step(action)
-        print("step test new observation:\n", obs['observation'])    
-    """
+        rospy.loginfo("ACTION")
+        rospy.loginfo(action)
+        rospy.loginfo("NEW OBSERVATION:")
+        rospy.loginfo(obs['observation'])
+
+    ##############    
+    # reachable action for "ee" type
+    # action = np.array([0.0032298252917826176, -0.14821836352348328, 0.9403465390205383])
+    # action = np.array([0.001, 0.001, 0.001])
+    ##############  
